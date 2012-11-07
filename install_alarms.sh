026716e7c4e4b323967a8e197fd83382c4015ef2
Joseph Lutalo W <jlutalo@ds.co.ug>
2012-11-07 22:34:48 +0300

    chmod +x scripts after install

--- install_alarms.sh
+++ install_alarms.sh
@@ -4,6 +4,12 @@ mkdir -p $INSTALL_DIR
 cp ping.py $INSTALL_DIR
 cp ping_alert.py $INSTALL_DIR
 cp run_alarms.sh $INSTALL_DIR
+chmod +x $INSTALL_DIR/ping_alert.py
+chmod +x $INSTALL_DIR/run_alarms.sh
+echo
+echo--------------
+echo "Please ensure that the command notify-send is installed on your machine"
+echo--------------
 echo
 echo "You might now wish to run 'crontab -e' as a non-root user to setup cron to routinely run your alarm script" 
 echo "Here's a sample:"
